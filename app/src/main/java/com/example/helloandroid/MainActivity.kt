e9435ed1999fc0ff767748c75988e5eb60d6fe25
KurodaNc <122328141+KurodaNc@users.noreply.github.com>
2023-02-09 20:43:02 -0300

    Update MainActivity.kt
    alteracao pelo github para atualizar o projeto

--- app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 //comentario de alteracao 1.1
+//comentario do github
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
