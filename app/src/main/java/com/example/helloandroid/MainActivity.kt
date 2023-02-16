e9b59b39883b38602ed940963688585302ab707b
naoto <naoto.celso@gmail.com>
2023-02-15 21:07:32 -0300

    Criacao de novas telas e seus respectivos botoes, botoes declarados do MainActivity

--- app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.helloandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 //comentario de alteracao 1.1
 //comentario do github
@@ -9,5 +12,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btLogin).setOnClickListener(
+            startActivity(Intent(this,EsqueciSenhaActivity::class.java))
+        )
+
+        findViewById<TextView>(R.id.btEsqueciSenha).setOnClickListener(
+            startActivity(Intent(this,EsqueciSenhaActivity::class.java))
+        )
+
+        findViewById<Button>(R.id.btCadastrar).setOnClickListener(
+            startActivity(Intent(this,EsqueciSenhaActivity::class.java))
+        )
     }
 }
