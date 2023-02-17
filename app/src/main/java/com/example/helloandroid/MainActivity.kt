d985b15edb5f2a8968fbe85cfb56e1e8dc0aba9d
naoto <naoto.celso@gmail.com>
2023-02-16 21:03:08 -0300

    Execucao do aplicativo com as tela inicial, esqueci snha, cadastre e botao entar

--- app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -3,26 +3,33 @@ package com.example.helloandroid
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 
 //comentario de alteracao 1.1
 //comentario do github
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.btLogin).setOnClickListener(
-            startActivity(Intent(this,EsqueciSenhaActivity::class.java))
-        )
+        findViewById<Button>(R.id.btLogin).setOnClickListener(this)
+        findViewById<TextView>(R.id.btEsqueciSenha).setOnClickListener(this)
+        findViewById<TextView>(R.id.btCadastrar).setOnClickListener(this)
+    }
 
-        findViewById<TextView>(R.id.btEsqueciSenha).setOnClickListener(
-            startActivity(Intent(this,EsqueciSenhaActivity::class.java))
-        )
-
-        findViewById<Button>(R.id.btCadastrar).setOnClickListener(
-            startActivity(Intent(this,EsqueciSenhaActivity::class.java))
-        )
+    override fun onClick(view: View?) {
+        when(view?.id) {
+            R.id.btLogin -> {
+                startActivity(Intent(this,HomeActivity::class.java))
+            }
+            R.id.btEsqueciSenha -> {
+                startActivity(Intent(this,EsqueciSenhaActivity::class.java))
+            }
+            R.id.btCadastrar -> {
+                startActivity(Intent(this,CadastroActivity::class.java))
+            }
+        }
     }
 }
