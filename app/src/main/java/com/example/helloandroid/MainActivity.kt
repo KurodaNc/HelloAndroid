9578a129ad5f760891c231f7c50a670b6d976a18
naoto <naoto.celso@gmail.com>
2023-02-23 20:33:16 -0300

    inicio da funcao lambda, terminado

--- app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -3,37 +3,36 @@ package com.example.helloandroid
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
 
 //comentario de alteracao 1.1
 //comentario do github
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.btLogin).setOnClickListener(object: View.OnClickListener{
-            override fun onClick(view: View?){
-                startActivity(Intent(this@MainActivity,HomeActivity::class.java))
-            }
-        })
-        findViewById<TextView>(R.id.btEsqueciSenha).setOnClickListener(this)
-        findViewById<TextView>(R.id.btCadastrar).setOnClickListener(this)
+        findViewById<Button>(R.id.btLogin).setOnClickListener {
+            //delega o tratamento para o metodo correto
+            onClickLogin()
+        }
+        findViewById<TextView>(R.id.btEsqueciSenha).setOnClickListener{
+            onClickEsqueciSenha()
+        }
+        findViewById<TextView>(R.id.btCadastrar).setOnClickListener{
+            onClickCadastrar()
+        }
     }
 
-    override fun onClick(view: View?) {
-        when(view?.id) {
-            R.id.btLogin -> {
-                startActivity(Intent(this,HomeActivity::class.java))
-            }
-            R.id.btEsqueciSenha -> {
-                startActivity(Intent(this,EsqueciSenhaActivity::class.java))
-            }
-            R.id.btCadastrar -> {
-                startActivity(Intent(this,CadastroActivity::class.java))
-            }
-        }
+    //um metodo para cada evento aqui
+    private fun onClickLogin() {
+        startActivity(Intent(this,HomeActivity::class.java))
+    }
+    private fun onClickEsqueciSenha() {
+        startActivity(Intent(this,EsqueciSenhaActivity::class.java))
+    }
+    private fun onClickCadastrar(){
+        startActivity(Intent(this,CadastroActivity::class.java))
     }
 }
